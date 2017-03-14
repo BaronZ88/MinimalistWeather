4f3ff50e7aac5d36f4be82668f9b266fee7d8ffb
张磊 <baron.zhanglei@gmail.com>
2017-03-14 11:15:55 +0800

    解决Vector兼容性问题导致的NotFoundExecption

--- library/src/main/java/com/baronzhang/android/library/activity/BaseActivity.java
+++ library/src/main/java/com/baronzhang/android/library/activity/BaseActivity.java
@@ -1,12 +1,28 @@
 package com.baronzhang.android.library.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 /**
  * @author baronzhang (baron[dot]zhanglei[at]gmail[dot]com ==>> baronzhang.com)
  */
 public class BaseActivity extends AppCompatActivity {
+
+    /*
+     * 解决Vector兼容性问题
+     *
+     * First up, this functionality was originally released in 23.2.0,
+     * but then we found some memory usage and Configuration updating
+     * issues so we it removed in 23.3.0. In 23.4.0 (technically a fix
+     * release) we’ve re-added the same functionality but behind a flag
+     * which you need to manually enable.
+     *
+     * http://www.jianshu.com/p/e3614e7abc03
+     */
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
